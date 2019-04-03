f0f6c25c7c5f3c2b7fb99a7efa42a3bad38429a9
Ripal Patel <ripal212007@gmail.com>
2019-04-02 19:21:02 -0500

    done

---
+++ Calculator/CalculationEngine.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calculator
+{
+    public class CalculationEngine
+    {
+        public int DoMath(string stuffToCalculate)
+        {
+            try
+            {
+                //throw new NotImplementedException();
+                var splitNumbers = stuffToCalculate.Split('+')
+                    .Select(int.Parse);
+                //int firstNumber = int.Parse(splitNumbers[0]);
+                //int secondNumber = int.Parse(splitNumbers[1]);
+
+                //var result = 0;
+                //foreach (var number in splitNumbers)
+                //{
+                //    result += number;
+                //}
+
+                //return result;
+                return splitNumbers.Sum();
+            }
+            catch(FormatException)
+            {
+                throw new InvalidInputException(); 
+            }
+            
+        }
+    }
+}
---
+++ Calculator/InvalidInputException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    public class InvalidInputException: Exception
+    {
+        public InvalidInputException() : base("The input received was invalid.")
+        {
+        }
+    }
+}
